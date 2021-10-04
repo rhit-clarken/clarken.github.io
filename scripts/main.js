b2ab150aa7fdee5eaa514f3fc80fff5b249e4114
Endia Clark <78235607+rhit-clarken@users.noreply.github.com>
2021-10-03 22:38:34 -0400

    finished portfolio assignment

--- scripts/main.js
+++ scripts/main.js
@@ -6,25 +6,32 @@
  * Endia Clark
  */
 
-var rhit = rhit || {};
 
-rhit.main = function(){
+var isDarkMode = false;
+
+main = function(){
     console.log("Ready");
     let themeButton = document.querySelector("#theme");
+    if(themeButton.dataset.value == "dark"){
+        isDarkMode= true;
+    }
     themeButton.addEventListener("click", function(event){
         let currentTheme = themeButton.dataset.value;
-        rhit.changeTheme(currentTheme);
+        changeTheme(currentTheme, isDarkMode);
     });
 };
 
-rhit.changeTheme = function(currentTheme){
+changeTheme = function(currentTheme){
     if(currentTheme == "bright"){
         console.log(currentTheme);
+        isDarkMode = true;
         console.log("bright theme on");
         document.querySelector("body").setAttribute("class", "d-theme");
         document.querySelector("#theme").dataset.value = "dark";
         document.querySelector("#theme").innerHTML = "too dark?";
+        console.log(isDarkMode);
     } else{
+        isDarkMode = false;
         console.log("dark mode");
         document.querySelector("body").setAttribute("class", "l-theme");
         document.querySelector("#theme").dataset.value = "bright";
@@ -33,4 +40,15 @@ rhit.changeTheme = function(currentTheme){
     }
 };
 
-rhit.main();
+window.onbeforeunload = function(){
+    console.log("page loaded");
+    console.log(isDarkMode);
+    if(isDarkMode){
+        document.querySelector("body").setAttribute("class", "d-theme");
+    } else{
+        document.querySelector("body").setAttribute("class", "l-theme");
+
+    }
+}
+
+main();
