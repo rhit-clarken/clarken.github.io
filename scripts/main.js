95462a1200f14fbbb774db15a29edc01baf85543
Endia Clark <78235607+rhit-clarken@users.noreply.github.com>
2021-10-29 15:16:43 -0400

    added features

--- scripts/main.js
+++ scripts/main.js
@@ -7,32 +7,33 @@
  */
 
 
-var isDarkMode = false;
-
 main = function(){
     console.log("Ready");
     let themeButton = document.querySelector("#theme");
     if(themeButton.dataset.value == "dark"){
+        localStorage.setItem('data-item', 'dark');
         isDarkMode= true;
     }
     themeButton.addEventListener("click", function(event){
-        let currentTheme = themeButton.dataset.value;
-        changeTheme(currentTheme, isDarkMode);
+        // let currentTheme = themeButton.dataset.value;
+        changeTheme();
     });
 };
 
-changeTheme = function(currentTheme){
-    if(currentTheme == "bright"){
-        console.log(currentTheme);
-        isDarkMode = true;
-        console.log("bright theme on");
+changeTheme = function(){
+    if(localStorage.getItem('data-item') == "bright"){
+        // console.log(currentTheme);
+        // isDarkMode = true;
+        localStorage.setItem('data-item', 'dark');
+        // console.log("bright theme on");
         document.querySelector("body").setAttribute("class", "d-theme");
         document.querySelector("#theme").dataset.value = "dark";
         document.querySelector("#theme").innerHTML = "too dark?";
-        console.log(isDarkMode);
+        // console.log(isDarkMode);
     } else{
-        isDarkMode = false;
-        console.log("dark mode");
+        // isDarkMode = false;
+        localStorage.setItem('data-item', 'bright');
+        // console.log("dark mode");
         document.querySelector("body").setAttribute("class", "l-theme");
         document.querySelector("#theme").dataset.value = "bright";
         document.querySelector("#theme").innerHTML = "too bright?";
@@ -40,13 +41,16 @@ changeTheme = function(currentTheme){
     }
 };
 
-window.onbeforeunload = function(){
+window.onload = function(){
     console.log("page loaded");
-    console.log(isDarkMode);
-    if(isDarkMode){
+    if(localStorage.getItem('data-item') == 'dark'){
         document.querySelector("body").setAttribute("class", "d-theme");
+        document.querySelector("#theme").dataset.value = "dark";
+        document.querySelector("#theme").innerHTML = "too dark?";
     } else{
         document.querySelector("body").setAttribute("class", "l-theme");
+        document.querySelector("#theme").dataset.value = "bright";
+        document.querySelector("#theme").innerHTML = "too bright?";
 
     }
 }
