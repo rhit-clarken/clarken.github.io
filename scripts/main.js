1aae9d9b345ed721e71c82b24bc834e098a21d9f
Endia Clark <78235607+rhit-clarken@users.noreply.github.com>
2022-02-14 15:04:17 -0500

    updated portfolio site single page design

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,39 @@
  */
 
 
-main = function(){
+main = function () {
     console.log("Ready");
     let themeButton = document.querySelector("#theme");
-    if(themeButton.dataset.value == "dark"){
+    if (themeButton.dataset.value == "dark") {
         localStorage.setItem('data-item', 'dark');
-        isDarkMode= true;
+        isDarkMode = true;
     }
-    themeButton.addEventListener("click", function(event){
+    themeButton.addEventListener("click", function (event) {
         // let currentTheme = themeButton.dataset.value;
         changeTheme();
     });
+
+    // document.querySelectorAll(".tablinks").forEach(button => {
+    //     button.addEventListener('click', function (b) {
+    //         openTab(button);
+    //     })
+    // })
+
+    // https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
 };
 
-changeTheme = function(){
-    if(localStorage.getItem('data-item') == "bright"){
+changeTheme = function () {
+    if (localStorage.getItem('data-item') == "bright") {
         // console.log(currentTheme);
         // isDarkMode = true;
         localStorage.setItem('data-item', 'dark');
@@ -30,7 +48,7 @@ changeTheme = function(){
         document.querySelector("#theme").dataset.value = "dark";
         document.querySelector("#theme").innerHTML = "too dark?";
         // console.log(isDarkMode);
-    } else{
+    } else {
         // isDarkMode = false;
         localStorage.setItem('data-item', 'bright');
         // console.log("dark mode");
@@ -41,13 +59,35 @@ changeTheme = function(){
     }
 };
 
-window.onload = function(){
+openTab = function (event, id) {
+    // Declare all variables
+    var i, tabcontent, tablinks, activeIndex;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(id).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+window.onload = function () {
     console.log("page loaded");
-    if(localStorage.getItem('data-item') == 'dark'){
+    document.getElementById("defaultOpen").click();
+    if (localStorage.getItem('data-item') == 'dark') {
         document.querySelector("body").setAttribute("class", "d-theme");
         document.querySelector("#theme").dataset.value = "dark";
         document.querySelector("#theme").innerHTML = "too dark?";
-    } else{
+    } else {
         document.querySelector("body").setAttribute("class", "l-theme");
         document.querySelector("#theme").dataset.value = "bright";
         document.querySelector("#theme").innerHTML = "too bright?";
